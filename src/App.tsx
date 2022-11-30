91e571d90dcd75bac7e4f886231603027d7107dc
Dominik Mrówka <dmrowka7@gmail.com>
2022-11-30 11:23:34 +0100

    Add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+interface Person {
+  name: string,
+  height: number,
+}
 
 function App() {
+
+  const [data, setData] = useState<Person>();
+  const [number, setNumber] = useState<number>(1);
+  const [photo, setPhoto] = useState('https://picsum.photos/534/383')
+
+  useEffect( () => {
+    fetch(`https://swapi.dev/api/people/${number}/`)
+    .then((res) => res.json())
+    .then((ppl) => setData(ppl))
+  }, [number])
+
+  const handleClick = () => {
+    setNumber(prevState => prevState + 1)
+    setPhoto('https://picsum.photos/534/383')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      HELLO
+      <h1>{data && data.name}</h1>
+      <h2>{data && data.height}</h2>
+      <button onClick={() => handleClick()}>Button</button>
+      <img src={photo} alt="" />
     </div>
   );
 }
